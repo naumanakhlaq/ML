1df96922e251098e5aeba99303bf88023d818ef5
naumanakhlaq <naumanakhlaq@hotmail.com>
2018-02-01 06:32:14 +0000

    Created Doc

---
+++ ML LinReg.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 21:55:15 2018
+
+@author: nauman
+"""
+
+import numpy as np
+import pandas as pd
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.datasets as skd
+from lxml import html
+import requests
+
+# 5, 6
+v1 = 8
+v2 = 9
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/forest-fires/forestfires.csv'
+#names = ['Frequency', 'Angle of attack', 'Chord length', 'Free-stream velocity', 'SSDT', 'Scaled Sound Pressure']
+dataset = pd.read_csv(url, delim_whitespace = False)
+
+array = dataset.values
+X = array[:, v1]
+Y = array[:, v2]
+#plt.ylim(ymin=0, ymax=50)
+
+Xi = np.array(X, dtype=float)
+Yi = np.array(Y, dtype=float)
+
+plt.figure(figsize=(9.8, 6))
+plt.scatter(Xi, Yi)
+plt.plot(np.unique(Xi), np.poly1d(np.polyfit(Xi, Yi, 1))(np.unique(Xi))) #now try ML packages
+
+
+ax = list(dataset)
+
+plt.xlabel(ax[v1]) 
+plt.ylabel(ax[v2])
+
+plt.show()
+
+
+'''
+
+names = np.array(names)
+
+a = numpy.asarray(names)
+cwd = os.getcwd()
+numpy.savetxt(cwd+"dataa.csv", a, delimiter=",""")'''
